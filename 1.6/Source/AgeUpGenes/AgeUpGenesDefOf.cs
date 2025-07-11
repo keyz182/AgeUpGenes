010cc907078c94fe3af71559ae0d6ebb1e45c6ce
Kieran David Evans <keyz182@gmail.com>
2025-07-11 13:18:56 +0100

    Cookie cutter initial commit

---
+++ 1.6/Source/AgeUpGenes/AgeUpGenesDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AgeUpGenes;
+
+[DefOf]
+public static class AgeUpGenesDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+    
+    static AgeUpGenesDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(AgeUpGenesDefOf));
+}
